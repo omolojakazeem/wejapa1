d87a9b8d3c13e973f93059de3c11a42ebe456637
Kazeem Omoloja <omolojakazeem@gmail.com>
2020-07-26 20:28:38 +0100

    Second Commit

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ let thisSite = http.createServer(function (req, res) {
     res.end("Hello World, Welcome to WeJapa Internships");
   } else if (req.url == "/" && req.method == "POST") {
     let name = "";
-    res.end(`Hello, ${name}`);
+    req.on("data", (chunk) => {
+      name += chunk.toString();
+      // convert Buffer to string
+      name = JSON.stringify(name);
+      name = name.replace("name=", "");
+    });
+    req.on("end", () => {
+      res.end(`Hello ${name}, Welcome to WeJapa Internships`);
+    });
   }
 });
 
